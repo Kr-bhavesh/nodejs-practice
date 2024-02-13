958f03735424f7816f5092151c99c0fd928d0c3e
bhavesh-14 <bhavesh.b@knackroot.com>
2024-02-13 18:32:15 +0530

    put secret in env

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,14 +1,14 @@
+require('dotenv').config
 const jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
 const token = req.header('Authorization');
 if (!token) return res.status(401).json({ error: 'Access denied' });
 try {
- const decoded = jwt.verify(token, '9j399dn2ndn39jakaka');
+ const decoded = jwt.verify(token, process.env.SECRET);
  req.userId = decoded.userId;
  next();
  } catch (error) {
  res.status(401).json({ error: 'Invalid token' });
  }
  };
-
 module.exports = verifyToken;
